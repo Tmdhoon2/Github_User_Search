475381d8419d818e6fb2cd283b23928fc17aa52e
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-03 19:04:41 +0900

    🌱 :: 프로젝트 생성

---
+++ GithubUserSearch/app/src/main/java/com/example/githubusersearch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.githubusersearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
