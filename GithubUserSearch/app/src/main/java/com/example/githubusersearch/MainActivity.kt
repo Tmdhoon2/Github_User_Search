3535954308a219f73fbc44796d6b71f1f3eca93f
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-04 23:18:08 +0900

    🌧 :: 검색 전 결과창을 가려주는 기능 추가

--- GithubUserSearch/app/src/main/java/com/example/githubusersearch/MainActivity.kt
+++ GithubUserSearch/app/src/main/java/com/example/githubusersearch/MainActivity.kt
@@ -20,10 +20,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.ivwhite.bringToFront()
+
         binding.ivSearch.setOnClickListener(View.OnClickListener {
             RetrofitBuilder.api.user(binding.etuserId.text.toString()).enqueue(object : Callback<UserResponse> {
                 override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
                     if(response.isSuccessful){
+                        binding.ivwhite.setImageResource(0)
+
                         var response = response.body()!!
 
                         Glide.with(applicationContext)
@@ -35,13 +39,15 @@ class MainActivity : AppCompatActivity() {
                         binding.tvfollowers.setText(""+response.followers)
                         binding.tvfollowing.setText(""+response.following)
                         binding.tvemail.setText(response.email)
-                        binding.tvBio.setText(response.bio)
+                        binding.tvdes.setText(response.bio)
 
                         var input = response.created_at
 
                         var token = input.split('-')
                         var token1 = token[2].split('T')
                         binding.tvjoin.setText("Join : " + token[0] + "-" + token[1] + "-" + token1[0])
+
+                        binding.tvrepo.setText(""+response.public_repos)
 
                         binding.btUrl.setOnClickListener(View.OnClickListener {
                             var intent = Intent(Intent.ACTION_VIEW, Uri.parse(response.html_url))
