b80e9a151bd935035a9f177bb5cf123f6f21b112
Tmdhoon_2 <hks026naver@gmail.com>
2022-08-14 20:37:15 +0900

    🗿 :: Room Library Like Entity 추가

---
+++ GithubUserSearch/app/src/main/java/com/example/githubusersearch/LikeEntity.kt
@@ -0,0 +1,12 @@
+package com.example.githubusersearch
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "like")
+data class LikeEntity(
+    @PrimaryKey(autoGenerate = true)
+    var url: String,
+    var name: String
+
+)
